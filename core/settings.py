6e6c2191022984522fb1dc79a2b649a549bc10c8
Affan <affanarif74@gmail.com>
2024-01-18 18:53:29 +0500

    Reverted gunicorn

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '0.0.0.0', '[::1]', '3.19.76.232']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
